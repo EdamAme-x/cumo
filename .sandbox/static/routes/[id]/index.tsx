5bec83db47ec78e851656b144eecb12347c36c0e
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-26 22:02:54 +0900

    add

--- .sandbox/static/routes/[id]/index.tsx
+++ .sandbox/static/routes/[id]/index.tsx
@@ -3,6 +3,6 @@ import type { Context } from "@hono/hono";
 export function GET(c: Context) {
     return c.html(<>
         <h1>Hello, World! from [id]/index.ts</h1>
-        <p>params | id: {c.req.param("id")}</p>    
+        <p>params / id: {c.req.param("id")}</p>    
     </>);
 }
