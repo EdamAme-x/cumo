4b259cb09885a37c127cfc8e932de5d5852b34eb
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-27 23:06:42 +0900

    wip

--- .sandbox/static/routes/index.tsx
+++ .sandbox/static/routes/index.tsx
@@ -3,6 +3,5 @@ import type { CumoResponse } from "../../../packages/runtime/mod";
 export default function Index(): CumoResponse {
     return <>
         <h1>Hello, World! from index.ts</h1>
-        {(<p>a</p>).toString()}
     </>;
 }
